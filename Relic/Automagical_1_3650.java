9acffa76c8d2e4a6039c06edff1f16aa98026ad7
Bryce Cordill <brycecordill@gmail.com>
2017-09-11 16:30:51 -0700

    Add initial autonomous code (mostly for testing)

---
+++ Relic/Automagical_1_3650.java
@@ -0,0 +1,58 @@
+package org.firstinspires.ftc.teamcode.Relic;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.vuforia.Vuforia;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+
+/**
+ * Created by Bryce on 9/11/2017.
+ */
+
+@Autonomous(name="Automagical 1", group ="3650")
+public class Automagical_1_3650 extends LinearOpMode {
+
+    VuforiaLocalizer vuforia;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        //make camera view show up on screen
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+
+        parameters.vuforiaLicenseKey = "";
+
+        //use back camera
+        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+        this.vuforia = ClassFactory.createVuforiaLocalizer(parameters);
+
+        //Get image identification files
+        VuforiaTrackables relicTrackables = this.vuforia.loadTrackablesFromAsset("RelicVuMark");
+        VuforiaTrackable relicTemplate = relicTrackables.get(0);
+        relicTemplate.setName("relicVuMarkTemplate");
+
+        waitForStart();
+
+        //begin searching
+        relicTrackables.activate();
+
+        while(opModeIsActive()){
+
+            RelicRecoveryVuMark vuMark = RelicRecoveryVuMark.from(relicTemplate);
+            if (vuMark != RelicRecoveryVuMark.UNKNOWN) {
+                telemetry.addData("Key: ", vuMark);
+            }
+
+            else{
+                telemetry.addData("No key detected!", null);
+            }
+            telemetry.update();
+        }
+    }
+}
