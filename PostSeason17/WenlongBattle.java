3ec136a2efb095b129721ec5f848b1f36dc4686d
Bryce Cordill <brycecordill@gmail.com>
2017-09-11 10:35:30 -0700

    Fix package names

--- PostSeason17/WenlongBattle.java
+++ PostSeason17/WenlongBattle.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.BattleBots17;
+package org.firstinspires.ftc.teamcode.PostSeason17;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
